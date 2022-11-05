65c007c124f0c7f6f61c1fdfe18e4e451cb5022f
Emir Tubcbilek <emir360t@gmail.com>
2022-11-05 08:13:47 -0400

    Merge remote-tracking branch 'origin/main'

--- InteractiveMap/generate_map.py
+++ InteractiveMap/generate_map.py
@@ -1,15 +1,15 @@
 import folium
 from create_coordinates import get_lat_and_long
 
-places = {"id 0001":{"Argentina":{"idea":"where does Horacio Pagani live?", "comments":{"bob":"idk"}}, "creation":{"author": "jorge", "date": "novembre 5th 2022", "tags": ["life"], "likes":1}},
-          "id 0002":{"Malé": {"idea":"vacation", "comments": {"jeff": "nice place"}}, "creation":{"author": "ben", "date": "novembre 6th 2022", "tags": ["life"], "likes":85}},
-          "id 0003":{"Montreal, QC": {"idea":"congestion sur la ligne bleue", "comments": {"steven":"rien de nouveau"}}, "creation":{"author": "adam", "date": "novembre 7th 2022", "tags": ["transport", "commute", "life"], "likes":4}},
-          "id 0004":{"Tokyo, Japan": {"idea":"what are jdm cars", "comments": {"Han": "life"}}, "creation":{"author": "jamal", "date": "novembre 5th 2022", "tags": ["life", "jdm", "environment", "happiness"], "likes":900000}},
-          "id 0005":{"france": {"idea":"baguette", "comments": {"François": "un très bon repas"}}, "creation":{"author": "arthur", "date": "novembre 3rd 2022","tags": ["food", "happiness"], "likes":76}},
-          "id 0006":{"deutschland": {"idea":"what is the best beer", "comments": {"chad": "they're all good"}},  "creation":{"author": "josh", "date": "novembre 2nd 2022", "tags": ["happiness", "life"], "likes":60}},
-          "id 0007":{"ICELAND": {"idea":"how cold does it get", "comments":{"josh": "very"}},  "creation":{"author": "sergei", "date": "novembre 11th 2022", "tags": ["weather", "environment"], "likes":5}},
-          "id 0008":{"Ohio": {"idea":"how many potatoes are collected per day", "comments":{"franck": "lots"}},  "creation":{"author": "vlad", "date": "novembre 5th 2022", "tags": ["food", "environment", "economy", "business"], "likes":89}},
-          "id 0009":{"Montreal, QC": {"idea": "congestion sur la ligne orange", "comments": {"steven2": "rien de nouveau"}},  "creation":{"author": "jorge", "date": "novembre 8th 2022", "tags": ["transport"],"likes":86}}
+places = {"id0001":{"Argentina":{"idea":"where does Horacio Pagani live?", "comments":{"bob":"idk"}}, "creation":{"author": "jorge", "date": "novembre 5th 2022", "tags": ["life"], "likes":1}},
+          "id0002":{"Malé": {"idea":"vacation", "comments": {"jeff": "nice place"}}, "creation":{"author": "ben", "date": "novembre 6th 2022", "tags": ["life"], "likes":85}},
+          "id0003":{"Montreal, QC": {"idea":"congestion sur la ligne bleue", "comments": {"steven":"rien de nouveau"}}, "creation":{"author": "adam", "date": "novembre 7th 2022", "tags": ["transport", "commute", "life"], "likes":4}},
+          "id0004":{"Tokyo, Japan": {"idea":"what are jdm cars", "comments": {"Han": "life"}}, "creation":{"author": "jamal", "date": "novembre 5th 2022", "tags": ["life", "jdm", "environment", "happiness"], "likes":900000}},
+          "id0005":{"france": {"idea":"baguette", "comments": {"François": "un très bon repas"}}, "creation":{"author": "arthur", "date": "novembre 3rd 2022","tags": ["food", "happiness"], "likes":76}},
+          "id0006":{"deutschland": {"idea":"what is the best beer", "comments": {"chad": "they're all good"}},  "creation":{"author": "josh", "date": "novembre 2nd 2022", "tags": ["happiness", "life"], "likes":60}},
+          "id0007":{"ICELAND": {"idea":"how cold does it get", "comments":{"josh": "very"}},  "creation":{"author": "sergei", "date": "novembre 11th 2022", "tags": ["weather", "environment"], "likes":5}},
+          "id0008":{"Ohio": {"idea":"how many potatoes are collected per day", "comments":{"franck": "lots"}},  "creation":{"author": "vlad", "date": "novembre 5th 2022", "tags": ["food", "environment", "economy", "business"], "likes":89}},
+          "id0009":{"Montreal, QC": {"idea": "congestion sur la ligne orange", "comments": {"steven2": "rien de nouveau"}},  "creation":{"author": "jorge", "date": "novembre 8th 2022", "tags": ["transport"],"likes":86}}
           }
 
 def creat_map(places):
@@ -32,9 +32,6 @@ def creat_map(places):
         <p>likes: {places[id]["creation"]["likes"]}</p>
         <input type="button" onclick="readInputsForComments" value="Like">
         <p></p>
-        <script>
-            readInputsForComments()
-            </script>
         <p>Comments:</p>
         <ul>
             <comment>{places[id][place]["comments"]}</comment>
@@ -42,9 +39,9 @@ def creat_map(places):
         <p>Enter a comment</p>
         <input type="text" id="comment" name="user comment"><br><br>
         <input type="submit" onclick="readInputsForComments" value="Post comment">
-        <script></script>
+        
         """
-        file = f"idea{id}.html"
+        file = f"idea_{id}.html"
 
         with open(file, "w") as f:
             f.write(idea_html)
@@ -62,15 +59,16 @@ def creat_map(places):
             <p>Enter a comment</p>
             <input type="text" id="comment" name="user comment", placeholder="reply to {str(list(places[id][place]["comments"].keys())[-1])}"><br><br>
             <input type="submit" onclick="readInputsForComments" value="Post comment">
-            <script></script>
             </p>
-            <a href={file}>Click here to read more about the idea</a>
+            <center><a href={file}>Click here to read more about the idea</a></center>
                   
         """
-        iframe = folium.IFrame(html=html_on_map, width=300, height=300)
-        popup = folium.Popup(iframe, max_width=1650)
+        #iframe = folium.Html(html_on_map), width=300, height=300)
+        popup = folium.Popup(folium.Html(html_on_map, script=True, width=300, height=300), max_width=1650)
         loc = list(get_lat_and_long(place))
         folium.Marker(location=loc, popup=popup, tooltip=place).add_to(map)
 
     map.save("map.html")
     print("ready")
+
+creat_map(places)
