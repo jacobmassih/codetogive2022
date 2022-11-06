5061f6e2c24218b7312469aa6a9063b338442917
Emir Tubcbilek <emir360t@gmail.com>
2022-11-06 11:56:38 -0500

    clusters colors & scrollable div

--- map/views.py
+++ map/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
-from map.models import Comment, Topic
+from models import Comment, Topic
 from .forms import CommentForm, UploadForm
 import folium
 from folium.plugins import MarkerCluster
@@ -31,13 +31,47 @@ def get_lat_and_long(address):
 
 def create_map(topics):
     map = folium.Map(location=[20, 10], zoom_start=2, min_zoom=2)
-    marker_cluster = MarkerCluster(name="Topics").add_to(map)
+
+    cluster_function_js = """
+        function(cluster) {
+        var childCount = cluster.getChildCount(); 
+        var c = ' marker-cluster-';
+
+        if (childCount <= 3) {
+            c += 'small';
+        } else if (childCount < 5 && childCount > 3) {
+            c += 'medium';
+        } else {
+            c += 'large'
+        }
+        return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster' + c, iconSize: new L.Point(40, 40) });
+        }
+        """
+
+    marker_cluster = MarkerCluster(name="Topics", icon_create_function=cluster_function_js).add_to(map)
 
     for topic in topics:
         comments = Comment.objects.filter(topic_id=topic.id).values()
         # Generates the content read in the popup bubbles when a location is clicked
-        popupContent = f"""
+        popupContent = """
+        <style>
+            .comments {
+                    border: none;
+                    padding: 5px;
+                    font: 14px/16px sans-serif;
+                    width: 100%;
+                    height: 250px;
+                    overflow: scroll;
+                    }
+                    
+                    /* Scrollbar styles */
+                    ::-webkit-scrollbar {
+                    width: 12px;
+                    height: 12px;
+                    }
+            </style>""" + f"""
             <h3>{topic.title}</h3>
+            <div class="comments">
             <p>{topic.description} </p>
             <p>{topic.author} </p>
             <p>Date of creation: {topic.date}</p>
@@ -51,6 +85,7 @@ def create_map(topics):
             <hr>
             <p> {comment['author']}: {comment['comment']} [Status :  {comment['status']}] <p>
             """
+        popupContent += "</div>"
 
         popup = folium.Popup(folium.Html(
             popupContent, script=True, width=350), max_width=350, max_height=500)
