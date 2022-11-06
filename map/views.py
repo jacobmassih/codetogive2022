88643a2b6814fe715e67d168f272fe452430d878
Jacob Massih <massih.jacob@gmail.com>
2022-11-05 23:23:50 -0400

    map data now pulled from db

--- map/views.py
+++ map/views.py
@@ -1,23 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+
+from map.models import Topic
 from .forms import UploadForm
 import folium
 from folium.plugins import MarkerCluster
 import requests
-
-
-places = {"id0001": {"Argentina": {"idea": "where does Horacio Pagani live?", "comments": {"bob": "idk"}}, "creation": {"author": "jorge", "date": "novembre 5th 2022", "tags": ["life"], "likes": 1}},
-          "id0002": {"Malé": {"idea": "vacation", "comments": {"jeff": "nice place"}}, "creation": {"author": "ben", "date": "novembre 6th 2022", "tags": ["life"], "likes": 85}},
-          "id0003": {"Montreal, QC": {"idea": "congestion sur la ligne bleue", "comments": {"steven": "rien de nouveau"}}, "creation": {"author": "adam", "date": "novembre 7th 2022", "tags": ["transport", "commute", "life"], "likes": 4}},
-          "id0004": {"Tokyo, Japan": {"idea": "what are jdm cars", "comments": {"Han": "life"}}, "creation": {"author": "jamal", "date": "novembre 5th 2022", "tags": ["life", "jdm", "environment", "happiness"], "likes": 900000}},
-          "id0005": {"france": {"idea": "baguette", "comments": {"François": "un très bon repas"}}, "creation": {"author": "arthur", "date": "novembre 3rd 2022", "tags": ["food", "happiness"], "likes": 76}},
-          "id0006": {"deutschland": {"idea": "what is the best beer", "comments": {"chad": "they're all good"}},  "creation": {"author": "josh", "date": "novembre 2nd 2022", "tags": ["happiness", "life"], "likes": 60}},
-          "id0007": {"ICELAND": {"idea": "how cold does it get", "comments": {"josh": "very"}},  "creation": {"author": "sergei", "date": "novembre 11th 2022", "tags": ["weather", "environment"], "likes": 5}},
-          "id0008": {"Ohio": {"idea": "how many potatoes are collected per day", "comments": {"franck": "lots"}},  "creation": {"author": "vlad", "date": "novembre 5th 2022", "tags": ["food", "environment", "economy", "business"], "likes": 89}},
-          "id0009": {"Laval, QC": {"idea": "congestion sur la ligne orange", "comments": {"steven2": "rien de nouveau"}},  "creation": {"author": "jorge", "date": "novembre 8th 2022", "tags": ["transport"], "likes": 86}},
-          }
-
 
 API_TOKEN = "AIzaSyBkmSyt4ooNWKph1sJ-xq4Z2NpzspFnZNY"
 
@@ -36,29 +25,30 @@ def get_lat_and_long(address):
         return location["lat"], location["lng"]
 
 
-def create_map(places):
+def create_map(topics):
     map = folium.Map(location=[20, 10], zoom_start=2, min_zoom=2)
     home_map = "map.html"
     marker_cluster = MarkerCluster(name="clustered ideas").add_to(map)
-    for id in places.keys():
+    for topic in topics:
 
-        place = list(places[id].keys())[0]
-        idea = places[id][place]["idea"]
+        print(topic)
+        city = topic.city
+        topicTitle = topic.title
     # Generates the "idea" html file when a location is clicked
         idea_html = f"""
         <!DOCTYPE html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-        <title>{idea}</title>
-        <p>Author: {places[id]["creation"]["author"]}</p>
-        <p>Date of creation: {places[id]["creation"]["date"]}</p>
-        <p>Tags: {places[id]["creation"]["tags"]}</p>
-        <p>likes: {places[id]["creation"]["likes"]}</p>
+        <title>{topicTitle}</title>
+        <p>Author: {topic.author}</p>
+        <p>Date of creation: {topic.date}</p>
+        <p>Tags: {topic.label}</p>
+        <p>likes: {topic.likes}</p>
         <input type="button" onclick="readInputsForComments" value="Like">
         <p></p>
         <p>Comments:</p>
         <ul>
-            <comment>{places[id][place]["comments"]}</comment>
+            <comment></comment>
         </ul>
         <p>Enter a comment</p>
         <input type="text" id="comment" name="user comment"><br><br>
@@ -67,7 +57,7 @@ def create_map(places):
 
         """
 
-        file = f"idea_{id}.html"
+        file = f"idea_{topic}.html"
 
         # with open(file, "w") as f:
         #     f.write(idea_html)
@@ -76,14 +66,14 @@ def create_map(places):
         # Link is now working
 
         html_on_map = f"""
-            <h1> {idea}</h1>
+            <h1> {topicTitle}</h1>
             <p> Last comment posted:</p>
             <ul>
-                <comment>{str(list(places[id][place]["comments"].keys())[-1])+" posted: "+ str(list(places[id][place]["comments"].values())[-1])}</comment>
+                <comment></comment>
             </ul>
             </p>
             <p>Enter a comment</p>
-            <input type="text" id="comment" name="user comment", placeholder="reply to {str(list(places[id][place]["comments"].keys())[-1])}"><br><br>
+            <input type="text" id="comment" name="user comment", placeholder="reply to"><br><br>
             <input type="submit" onclick="readInputsForComments" value="Post comment">
             </p>
             <center><a href={file}>Click here to read more about the idea</a></center>
@@ -92,9 +82,9 @@ def create_map(places):
 
         popup = folium.Popup(folium.Html(
             html_on_map, script=True, width=300, height=350), max_width=300, max_height=350)
-        loc = list(get_lat_and_long(place))
+        loc = list(get_lat_and_long(city))
         folium.Marker(location=loc, popup=popup,
-                      tooltip=place).add_to(marker_cluster)
+                      tooltip=city).add_to(marker_cluster)
 
     folium.LayerControl().add_to(map)
 
@@ -102,7 +92,9 @@ def create_map(places):
 
 
 def index(request):
-    context = {'map': create_map(places), 'form': UploadForm}
+    topics = Topic.objects.all()
+
+    context = {'map': create_map(topics), 'form': UploadForm}
     if (request.method == 'POST'):
         form = UploadForm(request.POST)
         if form.is_valid():
