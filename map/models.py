02040f0339cd549f1009a5a2457f85f75d3a78eb
Jacob Massih <massih.jacob@gmail.com>
2022-11-05 21:08:31 -0400

    init db

--- map/models.py
+++ map/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    author = models.CharField(max_length=20)
+    comment = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=255)
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=20)
+    description = description = models.CharField(max_length=255)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    city = models.CharField(max_length=255)
+    label = models.CharField(max_length=255)
+    likes = models.BigIntegerField(default=0)
