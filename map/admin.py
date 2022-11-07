4a0775fe0a6fa90b0c92a735b8eb682bbe5e1348
JasonOwaida <jasonowaida@hotmail.com>
2022-11-06 21:52:21 -0500

    Added filters for admin page

--- map/admin.py
+++ map/admin.py
@@ -3,9 +3,12 @@ from .models import Comment, Topic
 
 class topic(admin.ModelAdmin):
     list_display = ["title", "author", "description", "date", "city", "label", "likes"]
+    list_filter = ['label', "city"]
 
 class comment(admin.ModelAdmin):
     list_display = ["author", "comment", "date", "status", "topic"]
+    list_filter = ['status']
+
 
 
 admin.site.register(Topic, topic)
